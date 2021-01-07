e6479ae10af55e2c2906090dca971c3c9b0c3ff2
Ching-Yang, Tseng <n7711245566@gmail.com>
2021-01-08 01:24:50 +0800

    rewrite embedded struct quiz with example test

--- embedded_json.go
+++ embedded_json.go
@@ -1,25 +1,24 @@
-package main
+package quiz
 
 import (
 	"encoding/json"
-	"fmt"
 	"time"
 )
 
-func main() {
+// Source: https://twitter.com/katie_hockman/status/1242527795777069062
+// The embedded time.Time implements MarshalJSON method
+// Thus, the anonymous struct t was bound with the method from embedded
+// unless it declares one itself.
+func EmbeddedJson() []byte {
 	t := struct {
 		time.Time
 		N int
 	}{
-		Time: time.Now(),
+		Time: time.Unix(1610039949, 0).UTC(),
 		N:    5,
 	}
 
 	m, _ := json.Marshal(t)
-	// Source: https://twitter.com/katie_hockman/status/1242527795777069062
-	// The embedded time.Time implements MarshalJSON method
-	// Thus, the anonymous struct t was bound with the method from embedded
-	// unless it delcares one itself.
+	return m
 	// "2020-03-25T12:08:53.806735+08:00"
-	fmt.Printf("%s", m)
 }
--- example_test.go
+++ example_test.go
@@ -10,3 +10,8 @@ func ExampleConstArray() {
 	fmt.Println(quiz.ConstArray())
 	// Output: 2
 }
+
+func ExampleEmbeddedJson() {
+	fmt.Printf("%s", quiz.EmbeddedJson())
+	// Output: "2021-01-07T17:19:09Z"
+}
